4b72608dd5d1ab4de50c24d40bfdbbb1900fa1dc
artus9033 <artus9033@gmail.com>
2025-06-12 17:17:02 +0200

    feat: output and format flags to export to file in a given format, impl of presentation layer logic

--- packages/license-kit/src/index.ts
+++ packages/license-kit/src/index.ts
@@ -8,6 +8,10 @@ import minimist from 'minimist';
 
 import { version } from '../package.json';
 
+import { serializeReport } from './serializeReport';
+import { type Format, validFormats } from './types/Format';
+import type { Output } from './types/Output';
+
 const args = minimist(process.argv.slice(2));
 
 if (args.help) {
@@ -21,6 +25,8 @@ Options:
   --copyleft: Check for copyleft licenses. Exits with error if strong copyleft licenses are found.
   --error-on-weak: Exit with error if weak copyleft licenses are found.
   --root: Path to the root of the React Native project. Defaults to the current working directory.
+  --format: Output format (default: 'json'). Supported formats: 'json', 'about-json' (AboutLibraries-compatible), 'text', 'markdown'.
+  --output: Where to write the output (default: 'stdout'). Supported: 'stdout', <path to output file>.
   --help: Show this help message.
   `);
   process.exit(0);
@@ -35,6 +41,21 @@ if (!fs.existsSync(packageJsonPath)) {
 }
 
 const licenses = scanDependencies(packageJsonPath);
+
+let format: Format = 'json';
+
+if (args.format) {
+  if (!validFormats.includes(args.format)) {
+    console.error(`Invalid format: ${args.format}. Supported formats: ${validFormats.join(', ')}`);
+    process.exit(1);
+  }
+
+  format = args.format;
+}
+
+let output: Output = args.output ?? 'stdout';
+
+const serializedResult = serializeReport({ licenses, format });
 
 if (args.copyleft) {
   const STRONG_COPYLEFT_LICENSES = [
@@ -88,5 +109,22 @@ if (args.copyleft) {
     console.log('✅ No copyleft licenses found');
   }
 } else {
-  console.log(licenses);
+  switch (output) {
+    case 'stdout':
+      console.log(serializedResult);
+      break;
+
+    default:
+      // output to a file
+      {
+        const outputPath = path.resolve(repoRootPath, output);
+
+        fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+        fs.writeFileSync(outputPath, serializedResult);
+
+        console.log(`Output written to ${outputPath}`);
+      }
+
+      break;
+  }
 }
