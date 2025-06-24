ab1a5e88102979d666da6810ddeb3921726bbd5d
Mateusz Mędrek <mateusz.medrek@callstack.com>
2025-06-24 20:41:52 +0200

    fix(#50): fix

--- scripts/e2e_android_emulator_build.sh
+++ scripts/e2e_android_emulator_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "List installed system images"
+sdkmanager --list_installed | grep system-images |grep x86_64
 yarn workspace react-native-legal-bare-example android:release
 TEST_STATUS=$?
 echo "Build completed with status $TEST_STATUS"
