30f8ce144a9c97edf1b048dbef9fc2182e9bda1b
artus9033 <artus9033@gmail.com>
2025-07-14 19:19:07 +0200

    chore: comply with biome in docs

--- docs/theme/index.tsx
+++ docs/theme/index.tsx
@@ -1,17 +1,20 @@
 import {
   Announcement,
+  Button,
   HomeBanner,
   HomeFeature,
   HomeFooter,
   HomeHero,
+  LinkCard,
   OutlineCTA,
   PrevNextPage,
-  LinkCard,
-  Button,
   VersionBadge,
 } from '@callstack/rspress-theme';
 import { useNavigate } from 'rspress/runtime';
-import { HomeLayout as RspressHomeLayout, Layout as RspressLayout } from 'rspress/theme';
+import {
+  HomeLayout as RspressHomeLayout,
+  Layout as RspressLayout,
+} from 'rspress/theme';
 
 const Layout = () => (
   <RspressLayout
@@ -28,16 +31,9 @@ const HomeLayout = () => {
       afterFeatures={
         <>
           <div className="docs-introduction-cta">
-            <span
-              onClick={() => {
-                navigate('/docs/introduction');
-              }}
-            >
-              {/* @ts-ignore-next-line -- invalid typings */}
-              <Button theme="alt" type="button" size="medium">
-                Not sure, which to choose?
-              </Button>
-            </span>
+            <Button theme="alt" type="button" href="/docs/introduction">
+              Not sure, which to choose?
+            </Button>
           </div>
 
           <HomeBanner href="https://callstack.com" />
@@ -49,6 +45,16 @@ const HomeLayout = () => {
 };
 
 // custom layouts and any components to be available via '@theme'
-export { Layout, HomeLayout, PrevNextPage, HomeFeature, HomeHero, LinkCard, Button, VersionBadge, Announcement };
+export {
+  Layout,
+  HomeLayout,
+  PrevNextPage,
+  HomeFeature,
+  HomeHero,
+  LinkCard,
+  Button,
+  VersionBadge,
+  Announcement,
+};
 // re-export the default theme components which are not overridden
 export * from 'rspress/theme';
