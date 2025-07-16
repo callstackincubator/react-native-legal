74a537fe5c6eca1114b5399c1659bf467684fc48
artus9033 <artus9033@gmail.com>
2025-07-16 19:22:33 +0200

    feat: repro commit

---
+++ rspress.config.ts
@@ -0,0 +1,27 @@
+import {pluginCallstackTheme} from '@callstack/rspress-theme/plugin';
+import * as path from 'node:path';
+import {defineConfig} from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'React Native Legal & License Kit',
+  description: 'React Native Legal & License Kit Documentation',
+  logoText: 'React Native Legal & License Kit',
+  icon: '/img/notice.png',
+  logo: '/img/notice.png',
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/callstackincubator/react-native-legal',
+      },
+    ],
+    footer: {
+      message: `Copyright © ${new Date().getFullYear()} Callstack Open Source`,
+    },
+  },
+  // base: '/react-native-legal/',
+  plugins: [pluginCallstackTheme()],
+  globalStyles: path.join(__dirname, 'styles/globalStyles.css'),
+});
---
+++ theme/index.tsx
@@ -0,0 +1,46 @@
+import {
+  Announcement,
+  Button,
+  HomeBanner,
+  HomeFeature,
+  HomeFooter,
+  HomeHero,
+  LinkCard,
+  OutlineCTA,
+  PrevNextPage,
+  VersionBadge,
+} from '@callstack/rspress-theme';
+import { Link, HomeLayout, Layout as RspressLayout } from 'rspress/theme';
+
+const Layout = () => (
+  <RspressLayout
+    // beforeNav={<Announcement href="./guide" message="Announcement Test" localStorageKey="" />}
+    afterOutline={<OutlineCTA href="https://callstack.com" />}
+  />
+);
+
+// const HomeLayout = () => {
+//   return (
+//     <RspressHomeLayout
+//       afterFeatures={
+//         <>
+//           <div className="docs-introduction-cta">
+//             <Link href="/docs/introduction">
+//               <Button theme="alt" type="button">
+//                 Not sure, which to choose?
+//               </Button>
+//             </Link>
+//           </div>
+
+//           <HomeBanner href="https://callstack.com" />
+//           <HomeFooter />
+//         </>
+//       }
+//     />
+//   );
+// };
+
+// custom layouts and any components to be available via '@theme'
+// export { Layout, HomeLayout, PrevNextPage, HomeFeature, HomeHero, LinkCard, Button, VersionBadge, Announcement };
+// re-export the default theme components which are not overridden
+export * from 'rspress/theme';
